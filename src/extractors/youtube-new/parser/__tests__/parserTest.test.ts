38a1f858a99e54c3bd4c6d192f6f89e7b4c30d50
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2022-12-15 01:14:07 +1300

    test: ✅ added & updated test suite

--- src/extractors/youtube-new/parser/__tests__/parserTest.test.ts
+++ src/extractors/youtube-new/parser/__tests__/parserTest.test.ts
@@ -1,5 +1,5 @@
 // TODO: add tests for the new parser
-import {applyObjectRule, ObjectRuleHelper, ArrayRuleHelper} from "../index";
+import {ObjectRuleHelper, ArrayRuleHelper, ObjectRuleParser} from "../index";
 import {objectRule, arrayRule} from "../types";
 import {utilityErrors} from "../../../../utils";
 
@@ -91,24 +91,141 @@ describe("Parser Tests", () => {
     describe("ObjectRule Tests", () => {
         describe("applyObjectRule error handling", () => {
             test("if applyObjectRule throws an error if the given rule is not an object rule", () => {
-                const notObjectRule: arrayRule = {
+                const notObjectRule: objectRule = {
                     type: "array",
-                } as arrayRule;
-                expect(() => applyObjectRule({}, notObjectRule)).toThrow(utilityErrors.VueTubeExtractorError);
+                } as unknown as objectRule;
+                const testParser = new ObjectRuleParser({}, notObjectRule);
+                expect(() => testParser.parse()).toThrow(utilityErrors.VueTubeExtractorError);
             });
             test("if applyObjectRule throws an error if the given rule is missing a properties key", () => {
-                const rule: objectRule = {
+                const missingPropertiesRule: objectRule = {
                     type: "object",
                 } as objectRule;
-                expect(() => applyObjectRule({}, rule)).toThrow(utilityErrors.VueTubeExtractorError);
+                const testParser = new ObjectRuleParser({}, missingPropertiesRule);
+                expect(() => testParser.parse()).toThrow(utilityErrors.VueTubeExtractorError);
             });
             test("if applyObjectRule throws an error if the given object is not an object", () => {
-                const rule: objectRule = {
+                const notObjRule: objectRule = {
                     type: "object",
                     properties: {},
                 };
-                expect(() => applyObjectRule([], rule)).toThrow(utilityErrors.VueTubeExtractorError);
-                expect(() => applyObjectRule("test" as unknown as object, rule)).toThrow(utilityErrors.VueTubeExtractorError);
+                const testParser = new ObjectRuleParser([], notObjRule);
+                const testParser2 = new ObjectRuleParser("test" as unknown as object, notObjRule);
+                expect(() => testParser.parse()).toThrow(utilityErrors.VueTubeExtractorError);
+                expect(() => testParser2.parse()).toThrow(utilityErrors.VueTubeExtractorError);
+            });
+        });
+        describe("applyObjectRule functionality", () => {
+            test("if applyObjectRule correctly applies a bare minimum rule", () => {
+                const basicRule: objectRule = {
+                    type: "object",
+                    properties: {
+                        test: {
+                            type: "string",
+                        },
+                    },
+                };
+                const testParser = new ObjectRuleParser({test: "test"}, basicRule);
+                expect(testParser.parse()).toEqual({test: "test"});
+            });
+            test("if applyObjectRule correctly applies a rule with a keymap", () => {
+                const keyMappedRule: objectRule = {
+                    type: "object",
+                    properties: {
+                        test: {
+                            type: "string",
+                        },
+                    },
+                    keymap: {
+                        test: "test2",
+                    }
+                };
+                const testParser = new ObjectRuleParser({test: "test"}, keyMappedRule);
+                expect(testParser.parse()).toEqual({test2: "test"});
+            });
+            test("if applyObjectRule correctly applies a rule with a condition", () => {
+                const conditionRule: objectRule = {
+                    type: "object",
+                    properties: {
+                        test: {
+                            type: "string",
+                        },
+                    },
+                    condition: (object) => object.test === "test",
+                };
+                const testParser = new ObjectRuleParser({test: "test2"}, conditionRule);
+                expect(testParser.parse()).toEqual(undefined);
+            });
+            describe('applyObjectRule properties tests', function () {
+                test("if applyObjectRule correctly applies a rule with a required property (strict mode)", () => {
+                    const missingRequiredRule: objectRule = {
+                        type: "object",
+                        strict: true,
+                        properties: {
+                            test: {
+                                type: "string",
+                                required: true,
+                            },
+                        },
+                    };
+                    const testParser = new ObjectRuleParser({}, missingRequiredRule);
+                    expect(() => testParser.parse()).toThrow(utilityErrors.VueTubeExtractorError);
+                });
+                test("if applyObjectRule correctly applies a rule with a required property (non-strict mode)", () => {
+                    const missingOptionalRule: objectRule = {
+                        type: "object",
+                        strict: false,
+                        properties: {
+                            test: {
+                                type: "string",
+                                required: true,
+                            },
+                        },
+                    };
+                    const testParser = new ObjectRuleParser({}, missingOptionalRule);
+                    expect(testParser.parse()).toEqual(undefined);
+                });
+                test("if applyObjectRule correctly drops a non-required property", () => {
+                    const extraPropertyRule: objectRule = {
+                        type: "object",
+                        strict: true,
+                        properties: {
+                            test: {
+                                type: "string",
+                                required: false,
+                            },
+                        },
+                    };
+                    const testParser = new ObjectRuleParser({test: 1}, extraPropertyRule);
+                    expect(testParser.parse()).toEqual(undefined);
+                });
+                test("if applyObjectRule correctly applies a rule with a default property", () => {
+                    const propertyWithDefaultRule: objectRule = {
+                        type: "object",
+                        strict: true,
+                        properties: {
+                            test: {
+                                type: "string",
+                                required: true,
+                                default: "test",
+                            },
+                            test2: {
+                                type: "string",
+                                required: false,
+                                default: "test2",
+                            },
+                            test3: {
+                                type: "rule",
+                                required: false,
+                                default: {
+                                    test: "test"
+                                }
+                            },
+                        },
+                    };
+                    const testParser = new ObjectRuleParser({}, propertyWithDefaultRule);
+                    expect(testParser.parse()).toEqual({test: "test", test2: "test2", test3: {test: "test"}});
+                });
             });
         });
     })
