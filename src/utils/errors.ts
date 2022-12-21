022d6dc6de7f63d87f57314e0c4d53314b00fa06
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2022-12-22 12:57:41 +1300

    feat: 🧑‍💻 fixed duplication on reportbug modifier function

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -39,18 +39,28 @@ export class ErrorMessages {
     static readonly unknownError = "An unknown error occurred. How did you even get here?";
     static readonly extractorNotReady = (extractorName: string) => `The extractor ${extractorName} is not ready. Please wait for it to finish initializing.`;
     static readonly invalidTypeGuard = (typeGuard: string) => `${typeGuard} is not a valid type guard. Please check the type guard you are using.`;
-    static readonly typeGuardMismatch = (expected: string, actual: string, key?: string) => ErrorMessages.appendAdditionalInfoIfPresent('Type guard mismatch', key ? ` when checking key ${key}` : '', '.', `Expected ${expected}, got ${actual}.`);
+    static readonly typeGuardMismatch = (expected: string, actual: string, key?: string) => ErrorMessages.appendAdditionalInfoIfPresent('Type guard mismatch', key ? `when checking key ${key}.` : '.', `Expected ${expected}, got ${actual}.`);
     static readonly ruleTypeMismatch = (expected: string, actual: string) => `Rule type mismatch. Expected ${expected}, got ${actual}.`;
     static readonly invalidRuleType = (expected: string, actual: string) => `Invalid rule type. Expected ${expected}, got ${actual}.`;
     static readonly missingValuesInRule = (ruleType: string, ...missingValues: string[]) => `${ruleType} rule is missing the following values: ${missingValues.join(', ')}.`;
-    static readonly reportBug = (baseMessage: string) => ErrorMessages.appendAdditionalInfoIfPresent(baseMessage, `This is a bug. Please report it on GitHub using the following link: https://github.com/VueTubeApp/VueTube-Extractor/issues/new?assignees=&labels=bug&template=bug-report.yml}.`)
-    static readonly missingRequired = (requiredType: string, requiredName: string, functionName?: string) => ErrorMessages.appendAdditionalInfoIfPresent(`${requiredType} ${requiredName} is required but is missing`, functionName ? ` when calling ${functionName}` : "", ".")
+
+    static reportBug(baseMessage: string) {
+        // Check if the base message already contains a link to the bug report page
+        const bugReportLink = "https://github.com/VueTubeApp/VueTube-Extractor/issues/new?assignees=&labels=bug&template=bug-report.yml"
+        if (baseMessage.includes(bugReportLink)) {
+            return baseMessage;
+        }
+        return ErrorMessages.appendAdditionalInfoIfPresent(baseMessage, `\nThis is a bug. Please report it on GitHub using the following link: ${bugReportLink}.`);
+    }
+
+    static readonly missingRequired = (requiredType: string, requiredName: string, functionName?: string) => ErrorMessages.appendAdditionalInfoIfPresent(`${requiredType} ${requiredName} is required but is missing`, functionName ? `when calling ${functionName}` : "", ".")
     static readonly notImplemented = (functionName: string) => `Function ${functionName} is not implemented. Please avoid using this function until it is implemented.`;
     static readonly subRuleError = (ruleName: string, error?: string) => ErrorMessages.appendAdditionalInfoIfPresent(`An was detected from when parsing a sub-rule of ${ruleName}`, error ? `:\n ${error}` : ".");
 
-    static appendAdditionalInfoIfPresent(message: string, ...additionalInfo: string[]): string {
+    static appendAdditionalInfoIfPresent(message: string, ...additionalInfo: (string | undefined)[]): string {
         if (additionalInfo.length > 0) {
-            return message + " " + additionalInfo.join(" ");
+            message += " " + additionalInfo.filter((info) => info !== undefined).join(" ");
+            return message
         }
         return message;
     }
