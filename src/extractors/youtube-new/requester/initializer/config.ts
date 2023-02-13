f2b9098d757d0e9ab0291f8e4ca0f285f52ee78d
Ethan <ethan@etekweb.net>
2023-02-12 21:18:12 -0500

    Begin new requester, implemented getDefaultConfig

---
+++ src/extractors/youtube-new/requester/initializer/config.ts
@@ -0,0 +1,54 @@
+import { Http, HttpResponse } from "@vuetubeapp/http";
+import { ClientName, YTClient } from "extractors/youtube-new/utils/types";
+import YT_CONSTANTS from "../../utils/constants";
+import { InitializationError } from "../../utils/errors";
+// import { YouTubeHTTPOptions, ytErrors, YtUtils } from "../../utils";
+
+export default class Config {
+  private client_: Partial<YTClient>;
+  private apiKey_: string;
+
+  get client(): Partial<YTClient> {
+    return this.client_;
+  }
+
+  get apiKey(): string {
+    return this.apiKey_;
+  }
+
+  parseResponse(response: HttpResponse) {
+    const rawData: Array<any> = JSON.parse(response.data.replace(")]}'", ""));
+    const data = rawData[0][2];
+    this.apiKey_ = data[1];
+    this.client_ = {
+      gl: data[0][0][1],
+      hl: data[0][0][0],
+      clientVersion: data[0][0][16],
+      clientName: YT_CONSTANTS.YTAPIVAL.CLIENT_WEB_Mobile as ClientName,
+      remoteHost: data[0][0][3],
+      visitorData: data[6],
+    };
+  }
+
+  /**
+   * Fetches the innertube key from the YouTube API.
+   * @returns {Promise<Config>} Class containing API key and initial client object
+   */
+  async getDefaultConfig(): Promise<Config> {
+    const response: void | HttpResponse = await Http.get({
+      url: `${YT_CONSTANTS.URL.YT_MOBILE}/sw.js_data`,
+      responseType: "text",
+    }).catch((err) => {
+      if (typeof err === "string") {
+        throw new InitializationError(err.toUpperCase());
+      } else if (err instanceof Error) {
+        throw new InitializationError(err.message);
+      }
+    });
+    if (!response || !response.data) {
+      throw new InitializationError("No response from YouTube API");
+    }
+    this.parseResponse(response);
+    return this;
+  }
+}
---
+++ src/extractors/youtube-new/utils/types.ts
@@ -0,0 +1,186 @@
+// import { imageData as ImageData, audioFormat as AudioFormat, videoFormat as VideoFormat } from "@types";
+
+// export {
+//   video as Video,
+//   videoCard as VideoCard,
+//   channelCard as ChannelCard,
+//   playlist as Playlist,
+//   pageSegment as PageSegment,
+//   genericPage as GenericPage,
+//   pageSegmentTypes as PageSegmentTypes,
+//   pageDivider as PageDivider,
+//   shelfSegment as ShelfSegment,
+//   searchResult as SearchResult,
+//   searchSuggestion as SearchSuggestion,
+//   pageElements as PageElements,
+//   thumbnail as Thumbnail,
+// } from "@types";
+
+// export interface PlayerResponse {
+//   playabilityStatus: {
+//     status: string;
+//     playableInEmbed?: boolean;
+//     reason?: string;
+//     contextParams: string;
+//     [x: string | number | symbol]: unknown;
+//   };
+//   streamingData: {
+//     expiresInSeconds: number;
+//     formats: Array<VideoFormat | AudioFormat>;
+//     adaptiveFormats: Array<VideoFormat | AudioFormat>;
+//   };
+//   playbackTracking: {
+//     videostatsPlaybackUrl: { baseUrl: string };
+//     videostatsDelayplayUrl: { baseUrl: string };
+//     videostatsWatchtimeUrl: { baseUrl: string };
+//     [x: string | number | symbol]: unknown;
+//   };
+//   captions?: {
+//     playerCaptionsTracklistRenderer: {
+//       captionTracks: [
+//         {
+//           baseUrl: string;
+//           name: object;
+//           vssId: string;
+//           languageCode: string;
+//           isTranslatable: boolean;
+//         }
+//       ];
+//       translationLanguages: [
+//         {
+//           languageCode: string;
+//           languageName: object;
+//         }
+//       ];
+//       audioTracks: Array<object>;
+//       defaultAudioTrackIndex: number;
+//     };
+//   };
+//   videoDetails: {
+//     videoId: string;
+//     title: string;
+//     lengthSeconds: number;
+//     keywords: Array<string>;
+//     channelId: string;
+//     isOwnerViewing: boolean;
+//     shortDescription: string;
+//     isCrawlable: boolean;
+//     thumbnail: {
+//       thumbnails: Array<ImageData>;
+//     };
+//     allowRatings: boolean;
+//     viewCount: number;
+//     author: string;
+//     isPrivate: boolean;
+//     isUnpluggedCorpus: boolean;
+//     isLiveContent: boolean;
+//     [x: string | number | symbol]: unknown;
+//   };
+//   microformat: {
+//     playerMicroformatRenderer: {
+//       lengthSeconds: number;
+//       ownerProfileUrl: string;
+//       externalChannelId: string;
+//       isFamilySafe: boolean;
+//       availableCountries: Array<string>;
+//       isUnlisted: boolean;
+//       viewCount: number;
+//       category?: string;
+//       publishDate: string;
+//       ownerChannelName: string;
+//       uploadDate: string;
+//       liveBroadcastDetails?: {
+//         isLiveNow: boolean;
+//         startTimestamp: string;
+//       };
+//       [x: string | number | symbol]: unknown;
+//     };
+//   };
+// }
+
+// interface RequesterConfig {
+//   data?: any;
+//   params?: any;
+// }
+
+// export interface BrowseConfig extends RequesterConfig {
+//   isContinuation?: boolean;
+// }
+
+// export type SearchFeatures =
+//   | "live"
+//   | "video4k"
+//   | "hd"
+//   | "subtitles"
+//   | "cc"
+//   | "video360"
+//   | "vr180"
+//   | "video3d"
+//   | "hdr"
+//   | "location"
+//   | "purchased";
+// export interface SearchFilter {
+//   uploadDate: "hour" | "day" | "week" | "month" | "year" | "all";
+//   order: "relevance" | "viewCount" | "rating" | "uploadDate";
+//   type: "video" | "playlist" | "channel" | "all";
+//   duration: "short" | "medium" | "long" | "all";
+//   features: Array<SearchFeatures>;
+// }
+
+// export type ParseTypes =
+//   | "videoDetail"
+//   | "homePage"
+//   | "searchSuggestions"
+//   | "searchResult";
+
+// export type UserConfig = {
+//   hl?: string;
+//   gl?: string;
+//   maxRetryCount?: number;
+// };
+
+export type ClientName = "ANDROID" | "IOS" | "WEB" | "MWEB";
+
+export type YTClient = {
+  gl: string;
+  hl: string;
+  deviceMake?: string;
+  deviceModel?: string;
+  userAgent: string;
+  clientName: ClientName;
+  clientVersion: string;
+  osName: "Android" | "iOS" | "Windows" | "MacOS" | "Linux" | "MWeb";
+  osVersion?: number;
+  platform: "MOBILE" | "DESKTOP";
+  remoteHost?: string;
+  clientFormFactor?: string;
+  visitorData?: string;
+};
+
+// export type YTContext = {
+//   client: YTClient;
+//   user: { lockedSafetyMode: boolean };
+//   request: { useSsl: boolean };
+// };
+
+// export type HTTPMetadata = {
+//   apiKey: string;
+//   context: YTContext;
+// };
+
+// // -- parsers -- //
+
+// export type YTVideoData = {
+//   player: PlayerResponse;
+//   next: object;
+// };
+
+// export interface Continuation {
+//   nextContinuationData: string;
+//   reloadContinuationData: string;
+// }
+
+// export interface YTPageParseResults<T> {
+//   page: T;
+//   Continuation?: Continuation;
+// }
