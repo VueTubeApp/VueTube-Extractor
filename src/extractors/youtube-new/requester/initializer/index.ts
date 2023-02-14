9366e82270695cc45446c1631445afa6f8cbb685
Ethan <ethan@etekweb.net>
2023-02-14 02:25:52 -0500

    Add Context to initializer (#67)
    * implemented getContext
    
    * improved error handling / more test cases for Config
    
    * test cases and some adjustments for Context

--- src/extractors/youtube-new/requester/initializer/index.ts
+++ src/extractors/youtube-new/requester/initializer/index.ts
@@ -1,9 +1,18 @@
 import Config from "./config";
+import YT_CONSTANTS from "../../utils/constants";
+import { YTContext } from "../../utils/types";
+import Context from "./context";
 
 export default class Initializer {
-  private config_: Config;
+  private config: Config;
+  private mWebContext: YTContext;
+  private appContext: YTContext;
 
   async init() {
-    this.config_ = await new Config().getDefaultConfig();
+    // Request API key from YouTube's Innertube API (as well as client object)
+    this.config = await new Config().getInnertubeConfig(YT_CONSTANTS.URL.YT_MOBILE);
+    // Generate context(s) for future requests
+    this.mWebContext = new Context("MOBILE_WEB").getContext(this.config.client);
+    this.appContext = new Context("MOBILE_APP").getContext(this.config.client);
   }
 }
