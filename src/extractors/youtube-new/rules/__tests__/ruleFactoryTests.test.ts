076a34c459e4c9175e96551978d92fd668cecc52
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2023-02-21 16:14:30 +1300

    test: ✅ added more tests to ruleFactory

--- src/extractors/youtube-new/rules/__tests__/ruleFactoryTests.test.ts
+++ src/extractors/youtube-new/rules/__tests__/ruleFactoryTests.test.ts
@@ -21,4 +21,24 @@ describe('ruleFactory tests', () => {
         factory.createRule(rule);
         expect(factory.getRule("test")).toBe(rule);
     });
+    test('ruleFactory should be able to create a rule with aliases', () => {
+        const factory = new ruleFactory();
+        const rule: Rule = {
+            type: "array",
+            name: "test",
+            aliases: ["test2"],
+            items: {
+                name: "test",
+                type: "object",
+                properties: {
+                    test: {
+                        type: "string",
+                    },
+                },
+            },
+        };
+        factory.createRule(rule);
+        expect(factory.getRule("test")).toBe(rule);
+        expect(factory.getRule("test2")).toBe(rule);
+    });
 })
