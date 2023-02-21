1ceda9c07003030a8723d83a0d0b5baa62ebf923
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2023-02-22 09:56:21 +1300

    ci: update coverage.yml

--- src/extractors/youtube-new/requester/initializer/config.ts
+++ src/extractors/youtube-new/requester/initializer/config.ts
@@ -37,7 +37,7 @@ export default class Config {
    * @returns {Promise<Config>} Class containing API key and initial client object
    */
   async getInnertubeConfig(
-    baseURL: string = YT_CONSTANTS.URL.YT_URL
+    baseURL: string = YT_CONSTANTS.URL.YT_MOBILE
   ): Promise<Config> {
     const response: void | HttpResponse = await Http.get({
       url: `${baseURL}/sw.js_data`,
