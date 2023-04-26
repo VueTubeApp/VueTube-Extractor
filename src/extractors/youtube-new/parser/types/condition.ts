f536ba3233cf405cb1519cfd70662aae7bf1bea6
Cyrus <cyrus.kuznetsov@gmail.com>
2023-04-26 13:24:04 +0500

    feat: add condition checks for flatten

--- src/extractors/youtube-new/parser/types/condition.ts
+++ src/extractors/youtube-new/parser/types/condition.ts
@@ -1,23 +1,15 @@
-import type { ObjectRule, ConditionalFn, ObjectRuleProps } from "./common";
+import type { ObjectRule, ConditionalFn, ObjectRuleProps, IndexTypeMap } from './common';
 
 type ConditionalKeysSet<Rule extends ObjectRule> = keyof {
-  [Key in keyof ObjectRuleProps<Rule> as ObjectRuleProps<Rule>[Key] extends { expected: unknown } ? Key : never]: unknown;
-}
+  [Key in keyof ObjectRuleProps<Rule> as ObjectRuleProps<Rule>[Key] extends { expected: IndexTypeMap<ObjectRuleProps<Rule>[Key]['type']> }
+    ? Key
+    : never]: unknown;
+};
 
-type HasConditionalKeysSet<Rule extends ObjectRule, First, Second> = 
-  ConditionalKeysSet<Rule> extends never ?
-  Second :
-  First;
+type HasConditionalKeysSet<Rule extends ObjectRule, First, Second> = ConditionalKeysSet<Rule> extends never ? Second : First;
 
-export type AppliedCondition<Rule, Type> = 
-  Rule extends ObjectRule ?
-  (
-    Rule extends { condition: ConditionalFn } ? 
-    Type | undefined :
-    HasConditionalKeysSet<
-      Rule, 
-      Type | undefined, 
-      Type
-    > 
-  ):
-  never;
+export type HasCondition<Rule extends ObjectRule, First, Second> = Rule extends { condition: ConditionalFn }
+  ? First
+  : HasConditionalKeysSet<Rule, First, Second>;
+
+export type AppliedCondition<Rule, Type> = Rule extends ObjectRule ? HasCondition<Rule, Type | undefined, Type> : never;
--- src/extractors/youtube-new/parser/types/default.ts
+++ src/extractors/youtube-new/parser/types/default.ts
@@ -1,3 +1,3 @@
 import type { PropertyRule, IndexTypeMap } from './common';
 
-export type PropDefaultSet<Prop extends PropertyRule, First, Second> = Prop extends { default: IndexTypeMap<Prop> } ? First : Second;
+export type PropDefaultSet<Prop extends PropertyRule, First, Second> = Prop extends { default: IndexTypeMap<Prop['type']> } ? First : Second;
